8024139bb92a5fa0e330b7c0cff26acaf6363f7f
Thiago Allisson <thiago.allisson@ufpi.edu.br>
2023-02-28 01:30:20 -0300

    Vitest e organizando testes

---
+++ src/test/transactions.spec.ts
@@ -0,0 +1,24 @@
+import { afterAll, beforeAll, describe, it } from 'vitest'
+import request from 'supertest'
+import { app } from '../app'
+
+describe('Transactions Routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+})
